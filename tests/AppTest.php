dcfa92055a87746370ccd7e8afdffe7fa427565a
Marc Guinea <develop.marcguinea@gmail.com>
2022-12-15 22:23:33 +0100

    feat: add logger interface

--- tests/AppTest.php
+++ tests/AppTest.php
@@ -8,6 +8,7 @@ use Flexible\App;
 use PHPUnit\Framework\TestCase;
 use Psr\Container\ContainerInterface;
 use Psr\Http\Message\ResponseFactoryInterface;
+use Psr\Log\LoggerInterface;
 
 final class AppTest extends TestCase
 {
@@ -19,12 +20,18 @@ final class AppTest extends TestCase
 
 		$this->app = new App(
 			$this->createMock(ContainerInterface::class),
-			$this->createMock(ResponseFactoryInterface::class)
+			$this->createMock(ResponseFactoryInterface::class),
+			$this->createMock(LoggerInterface::class)
 		);
 	}
 
 	public function testContainerResponse(): void
 	{
 		$this->assertInstanceOf(ContainerInterface::class, $this->app->container());
+	}
+
+	public function testVersion(): void
+	{
+		$this->assertEquals('dev', App::VERSION);
 	}
 }
